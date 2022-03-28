b43a75384d18df294fe51ed5cf55293bb033cf16
jeon-ji-yoon <rebegin15@gmail.com>
2022-03-28 17:06:46 +0900

    dependency setting

---
+++ src/main/java/agora/RaisonApplication.java
@@ -0,0 +1,13 @@
+package agora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaisonApplication.class, args);
+	}
+
+}
