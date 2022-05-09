5c1ed189c42b929a0b94e781b2d37bddc0e19752
jeon-ji-yoon <rebegin15@gmail.com>
2022-05-09 14:17:45 +0900

    회원목록 기능 구현

--- src/main/java/raison/web/UserController.java
+++ src/main/java/raison/web/UserController.java
@@ -2,15 +2,28 @@ package raison.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import raison.User;
+import java.util.List;
+import java.util.ArrayList;
 
 @Controller
 public class UserController {
 
+    List<User> users = new ArrayList<>();
+
     @PostMapping("/create")
     public String createUser(@ModelAttribute UserDataCarrier userDataCarrier, Model model) {
         System.out.println("userDataCarrier = " + userDataCarrier);
-        return "index";
+        users.add(userDataCarrier.toUser());
+        return "redirect:/user-list";
+    }
+
+    @GetMapping(path = "/user-list")
+    public String userList(Model model) {
+        model.addAttribute("users", users);
+        return "userList";
     }
 }
